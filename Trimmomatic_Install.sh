fdc4d49d53619c4196970d53fffc31247acaaff7
pabloati <57755931+pabloati@users.noreply.github.com>
2019-11-14 15:34:35 +0100

    Create Trimmomatic_Install.sh

---
+++ Trimmomatic_Install.sh
@@ -0,0 +1,7 @@
+#Download link for Trimmomatic
+wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip
+
+#Unzip the downloaded file
+unzip Trimmomatic-Src-0.38.zip
+
+
