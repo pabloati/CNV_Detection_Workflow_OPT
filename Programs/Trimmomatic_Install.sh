46321f17df00037ee18df686bc2bf5f5960b4bb4
pabloati <57755931+pabloati@users.noreply.github.com>
2019-11-15 16:29:02 +0100

    Update Trimmomatic_Install.sh

--- Programs/Trimmomatic_Install.sh
+++ Programs/Trimmomatic_Install.sh
@@ -2,6 +2,7 @@
 wget http://www.usadellab.org/cms/uploads/supplementary/Trimmomatic/Trimmomatic-0.39.zip
 
 #Unzip the downloaded file
-unzip Trimmomatic-Src-0.38.zip
+unzip Trimmomatic-Src-0.39.zip
 
-
+# add Trimmomatic to the Path
+export PATH=$PWD/Trimmomatic-0.39:${PATH}
