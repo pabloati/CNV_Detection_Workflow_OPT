3bd6defead09fb7bd1ea500d5ee391b124094fb2
pabloati <57755931+pabloati@users.noreply.github.com>
2019-11-26 12:42:17 +0100

    Update Filtering.sh

--- Scripts/Filtering.sh
+++ Scripts/Filtering.sh
@@ -6,7 +6,7 @@ done
 
 # Insert size 150-800 filter
 for i in $(ls *_noSup.bam); do
-  name=$(echo $i | cut -d '_' -f 2)
+  name=$(echo $i | cut -d '_' -f 1)
   samtools view -h $i | perl samparser.sh -e 50 --mm 2 50 --insert 150 800 -i - | samtools view -Sb - > "$name""_noSup_800_filtered.bam"
   done
 
