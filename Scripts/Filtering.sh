3cdfce893fb67bd1a005fe111a085716c8fc51ae
pabloati <57755931+pabloati@users.noreply.github.com>
2019-12-09 14:51:02 +0100

    Update Filtering.sh

--- Scripts/Filtering.sh
+++ Scripts/Filtering.sh
@@ -13,5 +13,5 @@ for i in $(ls *_noSup.bam); do
 # Insert size 150-10k filter
 for i in $(ls *_noSup.bam); do
   name=$(echo $i | cut -d '_' -f 1)
-   ~/Programs/samtools-1.9/samtools view -h $i | perl samparser.sh -e 50 --mm 2 50 --insert 150 800 -i - |  ~/Programs/samtools-1.9/samtools view -Sb - > "$name""_noSup_10k_filtered.bam"
+   ~/Programs/samtools-1.9/samtools view -h $i | perl samparser.sh -e 50 --mm 2 50 --insert 150 10000 -i - |  ~/Programs/samtools-1.9/samtools view -Sb - > "$name""_noSup_10k_filtered.bam"
   done
