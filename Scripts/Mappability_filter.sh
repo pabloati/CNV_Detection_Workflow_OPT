edc1da411bbbc53e5dff8e138439df9cef6e20f1
pabloati <57755931+pabloati@users.noreply.github.com>
2019-12-12 14:45:34 +0100

    Update Mappability_filter.sh

--- Scripts/Mappability_filter.sh
+++ Scripts/Mappability_filter.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 #Extracting overlaping k-mer subsequences as read sequences
-~/Programs/Seqbility/splitfa.c ~/Maize_RefGen/UnmaskedDNA/Zea_mays.AGPv4.dna.toplevel.fa | split -l 20000000
+~/Programs/Seqbility/splitfa ~/Maize_RefGen/UnmaskedDNA/Zea_mays.AGPv4.dna.toplevel.fa 35 | split -l 20000000
 
 #Aligning reads to the genome
-bwa aln -R 1000000 -O 3 -E 3 Zea_mays.AGPv4.dna.toplevel.fa ??????
+bwa aln -R 1000000 -O 3 -E 3 ~/Maize_RefGen/UnmaskedDNA/Zea_mays.AGPv4.dna.toplevel.fa > B73v4.sai
+
+#Coverting .sai into .sam
+bwa samse ~/Maize_RefGen/UnmaskedDNA/Zea_mays.AGPv4.dna.toplevel.fa B73v4.sai > B73v4.sam
 
 #Generate rawMask
-gzip -dc ?? | ~/Programs/Seqbility/gen_raw_mask.pl > rawMask_35.fa
+~/Programs/Seqbility/gen_raw_mask.pl B73v4.sam > rawMask_35.fa
 
 #Generating the final mask
-~/Programs/Seqbility/gen_mask.c -l 35 -r 0.5 rawMask_35.fa > mask_35_50.fa
+~/Programs/Seqbility/gen_mask -l 35 -r 0.5 rawMask_35.fa > mask_35_50.fa
 
-~/Programs/Seqbility/apply_mask_s.c mask_35_50.fa ~/Maize_RefGen/UnmaskedDNA/Zea_mays.AGPv4.dna.toplevel.fa > genome.mask.fa
+~/Programs/Seqbility/apply_mask_s mask_35_50.fa ~/Maize_RefGen/UnmaskedDNA/Zea_mays.AGPv4.dna.toplevel.fa > genome.mask.fa
 
-~/Programs/Seqbility/apply_mask_l.c mask_35_50.fa in.list > out.list 
+~/Programs/Seqbility/apply_mask_l mask_35_50.fa in.list > out.list 
