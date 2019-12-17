8a48848a4dc9e9544a3b70fa1080cf665657dfa1
pabloati <57755931+pabloati@users.noreply.github.com>
2019-12-17 11:28:52 +0100

    Update CNVnator.sh

--- Scripts/CNV-calling/CNVnator/CNVnator.sh
+++ Scripts/CNV-calling/CNVnator/CNVnator.sh
@@ -1,27 +1,29 @@
+#!/bin/bash
 # Here are 5 steps for CNVnator and I done them one by one, when using please remove '#' step by step
 
 # Step1: extracting read mapping from bam/sam files
 for i in $(ls ~/Files/Pablo_Results/Filtering/*noSup*.bam); do
-  name=$(echo $i | cut -d "/" -f 7| cut -d "." -f 1); echo $name
- # ~/Programs/CNVnator/cnvnator -root "$name"".root" -tree $i &
-  #done
+  name=$(echo $i | cut -d "/" -f 8| cut -d "." -f 1)
+  ~/Programs/CNVnator/cnvnator -root "$name"".root" -tree $i 
+  done
 
 # Step2: generate a histogram
-#for i in $(ls *root); do
- # ~/Programs/CNVnatorcnvnator -root $i -his 1000 -d ~/Maize_RefGen/MaskedDNA &
- # done
-
+for i in $(ls *root); do
+  ~/Programs/CNVnator/cnvnator -root $i -his 1000 -d ~/Maize_RefGen/MaskedDNA/Zea_mays.AGPv4.dna_rm.toplevel.fa 
+ 
 # Step3: calculating statistics
-#do ./cnvnator -root $i -stat 1000 &
-
-#done
+  ~/Programs/CNVnator/cnvnator -root $i -stat 1000 
 
 # Step4: ReadDepth signal partitioning
-#do ./cnvnator -root $i -partition 1000 &
-
-#done
+  ~/Programs/CNVnator/cnvnator -root $i -partition 1000 
 
 # Step5: CNV calling
-#do name=$(echo $i | cut -d "." -f1)
-#./cnvnator -root $i -call 1000 > "$name""_1000_CNVnator.cnv" &
-#done
+name=$(echo $i | cut -d "." -f 1)
+  ~/Programs/CNVnator/cnvnator -root $i -call 1000 > "$name""_1000_CNVnator.cnv" 
+  done
+
+#Conversion of .cnvfile to common format 
+for i in $(ls *.cnv); do 
+  name=$(echo $i | cut -d '_' -f 1,3)
+  python /data/home/ge97xak/Programs/TCAG-WGS-CNV-workflow/convert_CNV_calls_to_common_format.py $i CNVnator > "$name""_1000_comformat_CNVnator.txt"
+  done
